ae26a4a05983976366c2d4b3d40782d035ee55fa
kuoruan <kuoruan@qq.com>
2015-05-15 02:10:38 +0800

    0.3

---
+++ src/com/buaa/utils/ChatWindowManager.java
@@ -0,0 +1,33 @@
+package com.buaa.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.buaa.domain.User;
+import com.buaa.view.ChatWindow;
+
+public class ChatWindowManager {
+    private static List<ChatWindow> list = new ArrayList<ChatWindow>();
+
+    public static void addChatWindow(ChatWindow chatWindow) {
+        list.add(chatWindow);
+    }
+
+    public static boolean isOpened(User user) {
+        for (ChatWindow chatWindow : list) {
+            if (chatWindow.getTarget().equals(user)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ChatWindow getOpened(User user) {
+        for (ChatWindow chatWindow : list) {
+            if (chatWindow.getTarget().equals(user)) {
+                return chatWindow;
+            }
+        }
+        return null;
+    }
+}
